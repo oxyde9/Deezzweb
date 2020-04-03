b10ccdd85d30ead88e77410eac7d9e07deeb159c
Rui <ruiduarte617@gmail.com>
2020-04-03 16:37:57 +0200

    Finish

---
+++ src/components/Home.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import fetchJsonp from 'fetch-jsonp';
+import Track from './Track';
+import FavService from '../FavService'
+function Home(props) {
+    const [title, setTitle] = useState('');
+    const [orderBy, setOrderBy] = useState('ALBUM_ASC');
+    const [musics, setMusics] = useState([]);
+    const music = props.music;
+    function changeTitle(event) {
+        setTitle(event.target.value);
+    }
+  function changeOrderBy(event) {
+        setOrderBy(event.target.value);
+    }
+    function changeMusics(event) {
+        setMusics(event.target.value);
+    }
+    function onSearch(event) {
+        event.preventDefault(); // Empêche le navigateur de recharger la page
+ 
+        const encodedTitle = encodeURIComponent(title);
+     
+        fetchJsonp(
+    `https://api.deezer.com/search?q=${encodedTitle}&order=${orderBy}&output=jsonp`
+        )
+        .then(res => res.json())
+        .then(data => data.data)
+        .then(musics => {
+            setMusics(musics);
+        });
+    
+    }
+    function onFavorites(music) {
+        FavService.toggleFavorite(music);
+        setMusics([...musics]);
+        
+    }
+    
+ 
+    return (
+        <main className="container mt-3">
+        <h1>Recherche</h1>
+        <p>Recherchez un titre sur Deezer en utilisant le formulaire suivant :</p>
+        <hr/>
+        <form onSubmit={onSearch}>
+            <div className="row">
+                <label htmlFor="searchText" className="col-sm-2 col-form-label text-right">Titre&nbsp;:</label>
+                <div className="col-sm-4">
+                    <input type="text" className="form-control" id="searchText"
+                        placeholder="Eminem, Armin Van Buuren, Rihanna, ..." onChange={changeTitle}/>
+                </div>
+                <label htmlFor="searchText" className="col-sm-2 col-form-label text-right">Trier par :</label>
+                <div className="col-sm-2">
+                    <select id="order" className="custom-select" onChange={changeOrderBy}>
+                        
+                        <option value="ALBUM_ASC">Album</option>
+                        <option value="ARTIST_ASC">Artiste</option>
+                        <option value="TRACK_ASC">Musique</option>
+                        <option value="RANKING">Les plus populaires</option>
+                        <option value="RATING_ASC">Les mieux notés</option>
+
+                    </select>
+                </div>
+                <div className="col-sm-2 text-right"  onChange={changeMusics}>
+                    <input type="submit" className="btn btn-primary" value="Go"/>
+                </div>
+            </div>
+        </form>
+        <hr/>
+        {/* <h3>Aucun résultat pour cette recherche ...</h3> */}
+        <h2>Résultats</h2>
+        <div className="card-group search-results">
+    {musics.map(music => (
+        <Track key={music.id} music={music} onClick={onFavorites} isFavorite={FavService.isFavorite(music)}/>
+    ))}
+</div>
+
+    </main>
+    );
+    
+}
+
+export default Home;
